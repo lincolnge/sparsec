e14685644f13a30bff0094a2bcbeda8a54da874d
March Liu <march.liu@gmail.com>
2015-03-05 17:08:55 +0800

    project init

---
+++ sparsec/sparsec/sparsec.h
@@ -0,0 +1,19 @@
+//
+//  sparsec.h
+//  sparsec
+//
+//  Created by Mars Liu on 15/3/4.
+//  Copyright (c) 2015年 Dwarf Artisan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for sparsec.
+FOUNDATION_EXPORT double sparsecVersionNumber;
+
+//! Project version string for sparsec.
+FOUNDATION_EXPORT const unsigned char sparsecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <sparsec/PublicHeader.h>
+
+
